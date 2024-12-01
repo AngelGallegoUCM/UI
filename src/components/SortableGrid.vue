e360a7791b6965a21c08f284ef292232965c2b54
hwenj <146937278+hwenj@users.noreply.github.com>
2024-12-01 13:37:48 +0100

    badge

--- src/components/SortableGrid.vue
+++ src/components/SortableGrid.vue
@@ -28,7 +28,7 @@
           </template>
           <template v-else-if="col.type == 'ObjectIds'">
             <span v-for="o in entry[col.key].map(v => gState.resolve(v))" :key="o.id"
-              class="small"
+              class="badge bg-info text-dark me-1"
               :data-bs-toggle="col.title ? 'tooltip' : null" 
               :data-bs-html="col.title ? true : null"
               :data-bs-title="col.title ? col.title(entry, col.key, o) : null">
